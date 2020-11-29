c7965a5b82f57b38ccc85d11e7d0c505511b7761
Philip Damra <pdamra@dealerinspire.com>
2020-11-29 02:21:57 -0700

    add dealership

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,42 @@
+export const addDealershipHandler = async (event: any = {}): Promise<any> => {
+  console.log("AddDealership Invoked: %j", { event });
+  const id = `${Date.now()}`;
+  const {
+    arguments: {
+      input: {
+        currentDealership = "",
+        groupId = "",
+        name = "",
+        street = "",
+        city = "",
+        state = "",
+        country = "",
+        zip = "",
+        url = "",
+        phone = "",
+        brand = "",
+        slug = "",
+        activation = "",
+      } = {},
+    } = {},
+  } = event;
+  return {
+    id,
+    currentDealership,
+    groupId,
+    name,
+    street,
+    city,
+    state,
+    country,
+    zip,
+    url,
+    phone,
+    brand,
+    slug,
+    activation,
+  };
+};
 export const addGroupHandler = async (event: any = {}): Promise<any> => {
   console.log("AddGroup Invoked: %j", { event });
   const id = `${Date.now()}`;
